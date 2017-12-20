eed6e929b73d057bfe15e693adb709fc823d9733
Timur <timamamed07@gmail.com>
2017-12-20 15:24:08 +0300

    add reverse_lexicographically_sort_test

--- test_ip_filter.cpp
+++ test_ip_filter.cpp
@@ -1,10 +1,11 @@
-#define BOOST_TEST_MODULE test_simple
+#define BOOST_TEST_MODULE ip_filter_test
 
 #include <boost/test/unit_test.hpp>
 #include <vector>
 
 #include "version.h"
 #include "filters.h"
+#include "ip_pool_struct.h"
 
 BOOST_AUTO_TEST_SUITE(Test_IP_Filter)
 
@@ -15,27 +16,27 @@ BOOST_AUTO_TEST_CASE(version_test)
 
 BOOST_AUTO_TEST_CASE(reverse_lexicographically_sort_test)
 {
-    std::vector<std::vector<std::string> > vec1, vec2;
-    std::vector<std::string> vec1_temp, vec2_temp, vec3_temp, vec4_temp;
-    vec1_temp.push_back("213");
-    vec1_temp.push_back("219");
-    vec1_temp.push_back("115");
-    vec1_temp.push_back("76");
+    IP_Pool vec1, expected;
+    IP vec1_temp, vec2_temp, vec3_temp, vec4_temp;
+    vec1_temp.emplace_back(std::string("213"));
+    vec1_temp.emplace_back(std::string("219"));
+    vec1_temp.emplace_back(std::string("115"));
+    vec1_temp.emplace_back(std::string("76"));
 
-    vec2_temp.push_back("213");
-    vec2_temp.push_back("213");
-    vec2_temp.push_back("103");
-    vec2_temp.push_back("190");
+    vec2_temp.emplace_back(std::string("213"));
+    vec2_temp.emplace_back(std::string("213"));
+    vec2_temp.emplace_back(std::string("103"));
+    vec2_temp.emplace_back(std::string("190"));
 
-    vec3_temp.push_back("213");
-    vec3_temp.push_back("213");
-    vec3_temp.push_back("102");
-    vec3_temp.push_back("167");
+    vec3_temp.emplace_back(std::string("213"));
+    vec3_temp.emplace_back(std::string("213"));
+    vec3_temp.emplace_back(std::string("102"));
+    vec3_temp.emplace_back(std::string("167"));
 
-    vec4_temp.push_back("213");
-    vec4_temp.push_back("213");
-    vec4_temp.push_back("102");
-    vec4_temp.push_back("166");
+    vec4_temp.emplace_back(std::string("213"));
+    vec4_temp.emplace_back(std::string("213"));
+    vec4_temp.emplace_back(std::string("102"));
+    vec4_temp.emplace_back(std::string("166"));
 
     vec1.push_back(vec3_temp);
     vec1.push_back(vec1_temp);
@@ -43,11 +44,15 @@ BOOST_AUTO_TEST_CASE(reverse_lexicographically_sort_test)
     vec1.push_back(vec2_temp);
     filter(vec1);
 
-    vec2.push_back(vec1_temp);
-    vec2.push_back(vec2_temp);
-    vec2.push_back(vec3_temp);
-    vec2.push_back(vec4_temp);
-    
-    BOOST_CHECK_EQUAL_COLLECTIONS(vec1.begin(), vec1.begin(), vec2.begin(), vec2.begin());
+    expected.push_back(vec1_temp);
+    expected.push_back(vec2_temp);
+
+    expected.push_back(vec4_temp);
+    expected.push_back(vec3_temp);
+
+    BOOST_REQUIRE_EQUAL(vec1.size(), expected.size());
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(vec1.begin(), vec1.begin(), expected.begin(), expected.begin());
 }
+
 BOOST_AUTO_TEST_SUITE_END()
